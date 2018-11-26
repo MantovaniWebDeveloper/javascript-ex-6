102690ab4cc07066a4241f2c50a9af80ca38b216
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-26 14:08:03 +0100

    init progetto es1 con logica scritta in italiano

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//far apparire 5 allert
+
+// impostare valore random ad ogni allert
+
+//dopo 30 secondi deve pareire un prompt alla volta (5)
+
+//confrontare se i numeri dell'utente inseriti sono uguali a quelli
+//della cpu
+
+//ritornando quanti numeri sono stati indovinati e quali
