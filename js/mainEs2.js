4f8f8bed58afe4d966fea44366a935ebd3ccdaa3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-26 15:25:12 +0100

    inseriti i prompt e stampato su html

--- js/mainEs2.js
+++ js/mainEs2.js
@@ -1,6 +1,7 @@
-//chiedo al utente nome e sess0
-
+//chiedo al utente nome e sesso
+var nomeUtente = prompt("Digita il tuo nome");
+var sessoUtente = prompt("Digita il tuo sesso");
 //stampo sul html il nome
-
+document.getElementById("nomeUtenteBox").innerHTML = nomeUtente;
 //modifico lo stile color del nome in base al sesso, azzurro uomo rosa
 //donna
