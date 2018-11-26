f80eedfebf4a99abe01ff045bb940195eacf8843
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-26 15:41:20 +0100

    finito

--- js/mainEs2.js
+++ js/mainEs2.js
@@ -2,6 +2,20 @@
 var nomeUtente = prompt("Digita il tuo nome");
 var sessoUtente = prompt("Digita il tuo sesso");
 //stampo sul html il nome
-document.getElementById("nomeUtenteBox").innerHTML = nomeUtente;
-//modifico lo stile color del nome in base al sesso, azzurro uomo rosa
-//donna
+document.getElementById("nomeUtenteBox").innerHTML += nomeUtente;
+
+if (sessoUtente == "donna" || sessoUtente == "femmina") {
+  //modifico lo stile color del nome in base al sesso, rosa
+  //donna
+  console.log("sono donna");
+  document.getElementById("nomeUtenteBox").className = "testoDonna";
+}
+else if (sessoUtente == "uomo" || sessoUtente == "maschio") {
+  //modifico lo stile color del nome in base al sesso, azzurro uomo
+  console.log("sono uomo");
+  document.getElementById("nomeUtenteBox").className = "testoUomo";
+}
+else {
+  console.log("Hai sbagliato ad inserire il tipo di sesso");
+  alert("ERRORE");
+}
