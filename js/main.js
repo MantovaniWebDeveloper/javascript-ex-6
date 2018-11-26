fddc320e06a09c9dd327fd610e9769d8ec00c1e2
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-26 14:21:55 +0100

    numeri random cpu fatto, inserimento numeri utente fatto. Prima versione

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 //far apparire 5 allert
-
-// impostare valore random ad ogni allert
-
-//dopo 30 secondi deve pareire un prompt alla volta (5)
-
+var numeriCpu = [];
+for (var i = 0; i < 5; i++) {
+  // impostare valore random ad ogni allert
+  var numeroCpu = numeroRandom(1,10);
+  console.log("numero random alert: " + numeroCpu);
+  alert(numeroCpu);
+  numeriCpu.push(numeroCpu);
+}
+//dopo 30 secondi deve partire un prompt alla volta (5)
+var secondi = 3;
+var numeriUtente = [];
+setTimeout(function() {
+  for (var i = 0; i < 5; i++) {
+    // impostare valore random ad ogni allert
+    var numeroUtente = parseInt(prompt("Inserisci il numero"));
+    console.log("numero utente alert: " + numeroUtente);
+    alert(numeroUtente);
+    numeriUtente.push(numeroUtente);
+  }
+}, secondi * 1000)
 //confrontare se i numeri dell'utente inseriti sono uguali a quelli
 //della cpu
 
